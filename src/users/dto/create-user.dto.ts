7da885edbc5ef929e0730bb51e41e07368e6077f
Leo Bazan <leonardo.picinin@ciss.com.br>
2024-01-20 17:42:43 -0300

    create user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,19 @@
-export class CreateUserDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString({ message: 'O nome deve ser uma string' })
+  @IsNotEmpty({ message: 'O nome é obrigatório' })
+  name: string;
+
+  @IsString({ message: 'O email deve ser uma string' })
+  @IsNotEmpty({ message: 'O email é obrigatório' })
+  email: string;
+
+  @IsString({ message: 'A senha deve ser uma string' })
+  @IsNotEmpty({ message: 'A senha é obrigatória' })
+  password: string;
+
+  @IsString({ message: 'A confirmação da senha deve ser uma string' })
+  @IsNotEmpty({ message: 'A confirmação da senha é obrigatória' })
+  confirmPassword: string;
+}
